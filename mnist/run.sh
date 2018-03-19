a2ab5f344f15db9be01f2ac388d16bc77385c8b2
Yibing Liu <zapplea@gmail.com>
2018-03-19 14:21:04 +1100

    pytorch model

---
+++ mnist/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --get-user-env
+#SBATCH --job-name="che313"
+#SBATCH --time=10:00:00
+#SBATCH --nodes=1
+#SBATCH --mem=200GB
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:1
+
+echo "loading"
+module load python/3.6.1
+module load cudnn/v6
+module load cuda/8.0.61
+module load pytorch/0.3.1-py36
+echo "loaded"
+python mnist
