5a7e8b982fb136b567a05f381d027154478ff383
Yibing Liu <zapplea@gmail.com>
2018-03-21 22:11:02 +1100

    pytorch model

--- mnist/mnist.py
+++ mnist/mnist.py
@@ -9,25 +9,34 @@ class DataGenerator:
         self.train_data, self.test_data = self.data_loader()
 
     def data_loader(self):
-        kwargs = {'num_workers': 1, 'pin_memory': True} if self.data_config['cuda'] else {}
-        train_data = tr.utils.data.DataLoader(
-            datasets.MNIST(self.data_config['data_filePath'], train=True, download=True,
+        # kwargs = {'num_workers': 1, 'pin_memory': True} if self.data_config['cuda'] else {}
+        # train_data = tr.utils.data.DataLoader(
+        #     datasets.MNIST(self.data_config['data_filePath'], train=True, download=True,
+        #                    transform=transforms.Compose([
+        #                        transforms.ToTensor(),
+        #                        transforms.Normalize((0.1307,), (0.3081,))
+        #                    ])),
+        #     batch_size=self.data_config['batch_size'], shuffle=True, **kwargs)
+        # test_data = tr.utils.data.DataLoader(
+        #     datasets.MNIST(self.data_config['data_filePath'], train=False,
+        #                    transform=transforms.Compose([
+        #                        transforms.ToTensor(),
+        #                        transforms.Normalize((0.1307,), (0.3081,))
+        #                    ])),
+        #     batch_size=self.data_config['batch_size'], shuffle=True, **kwargs)
+        train_data = datasets.MNIST(self.data_config['data_filePath'], train=True, download=True,
                            transform=transforms.Compose([
                                transforms.ToTensor(),
                                transforms.Normalize((0.1307,), (0.3081,))
-                           ])),
-            batch_size=self.data_config['batch_size'], shuffle=True, **kwargs)
-        test_data = tr.utils.data.DataLoader(
-            datasets.MNIST(self.data_config['data_filePath'], train=False,
+                           ]))
+        test_data = datasets.MNIST(self.data_config['data_filePath'], train=False,
                            transform=transforms.Compose([
                                transforms.ToTensor(),
                                transforms.Normalize((0.1307,), (0.3081,))
-                           ])),
-            batch_size=self.data_config['batch_size'], shuffle=True, **kwargs)
+                           ]))
         return train_data, test_data
 
     def feed_train(self):
-        print(type(self.train_data))
         data = enumerate(self.train_data)
         for id, (instances,labels) in data:
             return instances, labels
@@ -59,5 +68,6 @@ if __name__ == "__main__":
     data_config = {'batch_size':30,'cuda':True,
                    'data_filePath':'/media/data2tb1/yibing/nosqldb/tr_data/MNIST'}
     dg = DataGenerator(data_config)
-    instances, labels = dg.feed_train()
-    print(type(instances))
+    # instances, labels = dg.feed_train()
+    train_data,test_data = dg.data_loader()
+    print(type(train_data))
