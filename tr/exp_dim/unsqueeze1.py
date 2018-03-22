e335af2f308d32abd70dc34a5120dbd3be56f9c6
Yibing Liu <zapplea@gmail.com>
2018-03-22 12:53:41 +1100

    pytorch model

--- tr/exp_dim/unsqueeze1.py
+++ tr/exp_dim/unsqueeze1.py
@@ -1,6 +1,6 @@
 import numpy as np
 import torch as tr
 
-v = tr.FloatTensor(np.array(shape=(30,),dtype='float32'))
+v = tr.FloatTensor(np.zeros(shape=(30,),dtype='float32'))
 result = tr.unsqueeze(v,dim=1)
 print(result.size())
