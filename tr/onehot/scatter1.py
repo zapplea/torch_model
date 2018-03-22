763348a80e460c2cef1c9995e6df2d55cd7a9c54
Yibing Liu <zapplea@gmail.com>
2018-03-22 12:44:51 +1100

    pytorch model

--- tr/onehot/scatter1.py
+++ tr/onehot/scatter1.py
@@ -1,6 +1,6 @@
 import numpy as np
 import torch as tr
 import torch.nn.functional as F
-y_=tr.LongTensor(np.array([1,4,3,2,1,2,2,1,3,0],dtype='float32'))
+y_=tr.LongTensor(np.array([[1],[4],[3],[2],[1],[2],[2],[1],[3],[0]],dtype='float32'))
 result = tr.zeros(10,5).scatter_(1,y_,1)
 print(result)
