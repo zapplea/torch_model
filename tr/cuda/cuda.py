6558cce616e3aedfe20284f2407307aef6ae6ad6
Yibing Liu <zapplea@gmail.com>
2018-04-06 22:28:24 +1000

    add

--- tr/cuda/cuda.py
+++ tr/cuda/cuda.py
@@ -4,11 +4,14 @@ import torch.nn.functional as F
 class Net(tr.nn.Module):
     def forward(self,X):
         result = F.softmax(tr.autograd.Variable(X,requires_grad=False),dim=1)
+        print(result.get_device())
         return result
 
-module= Net()
-module.cuda(device=0)
-X = tr.ones((3,3))
-# X.cuda()
-outputs = module.forward(X)
-print(outputs.data.numpy())
+with tr.cuda.device(0):
+    module= Net()
+    module.cuda()
+    X = tr.ones((3,3))
+    X = X.cuda()
+    print(X.get_device())
+    outputs = module.forward(X)
+    print(outputs.data.numpy())
