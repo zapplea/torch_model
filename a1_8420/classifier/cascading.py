c8428d942008bdc9ef06321523a33c5ceeebdf93
Yibing Liu <zapplea@gmail.com>
2018-04-08 22:38:05 +1000

    cascade:debug

--- a1_8420/classifier/cascading.py
+++ a1_8420/classifier/cascading.py
@@ -90,8 +90,11 @@ class Cascading:
             score = model.forward(tr.autograd.Variable(X,requires_grad=False))
             print('loss')
             loss = self.cross_entropy_loss(score,tr.autograd.Variable(y_,requires_grad=False))
+            print('backward')
             loss.backward()
+            print('optim')
             optim.step()
+            print('one batch finish')
 
     def knn_matrix_generator(self,true_lables,pred_labels,X):
         knn_features = []
