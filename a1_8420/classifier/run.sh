83e3792500e1ed573860addec1b9069120480315
Yibing Liu <zapplea@gmail.com>
2018-04-08 21:57:32 +1000

    cascade:debug

--- a1_8420/classifier/run.sh
+++ a1_8420/classifier/run.sh
@@ -6,14 +6,13 @@
 #SBATCH --mem=1GB
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --gres=gpu:1
 #SBATCH --qos=express
 
 echo "loading"
 module load python/3.6.1
 module load cudnn/v7.1.2-cuda91
 module load cuda/9.1.85
-module load pytorch/0.3.1-py36-cuda9
+module load pytorch/0.3.1-py36-cuda91
 echo "loaded"
 
 python data_feeder.py
