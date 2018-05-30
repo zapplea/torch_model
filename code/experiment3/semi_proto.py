d3c59fe8ef045e629eb3ac1977d64214d5c8b008
Yibing Liu <zapplea@gmail.com>
2018-05-30 23:07:11 +1000

    add

--- code/experiment3/semi_proto.py
+++ code/experiment3/semi_proto.py
@@ -64,7 +64,7 @@ class Net(tr.nn.Module):
         """
         C = C.view(-1,1,self.nn_config['feature_height_dim'],self.nn_config['feature_width_dim'])
         # shape = (labels dim* k shot, cnn_feature_dim)
-        C = self.forwafrd_cnn(C)
+        C = self.forward_cnn(C)
         # shape = (labels dim*k shot, connect_feature_dim)
         C = self.forward_nonlinear(C)
         # shape = (labels dim, k shot, connect_feature_dim)
