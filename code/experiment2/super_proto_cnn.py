9db90c78fba3f32a5c5747ae0fb39f13f262ef1e
Yibing Liu <zapplea@gmail.com>
2018-05-30 18:29:14 +1000

    add

--- code/experiment2/super_proto_cnn.py
+++ code/experiment2/super_proto_cnn.py
@@ -266,8 +266,12 @@ class SuperPrototypicalNet:
         for X, y_ in test_data:
             if self.nn_config['cuda'] and tr.cuda.is_available():
                 X,y_,C = X.cuda(),y_.cuda(),C.cuda()
+            print('X: ',X.size())
+            print('C: ',C.size())
             X = tr.unsqueeze(X, dim=3)
             C = tr.unsqueeze(C, dim=3)
+            print('X: ',X.size())
+            print('C: ', C.size())
             score = module.forward_softmax(tr.autograd.Variable(X, requires_grad=False),
                                            tr.autograd.Variable(C, requires_grad=False))
             loss = module.cross_entropy_loss(score, tr.autograd.Variable(y_.long(), requires_grad=False))
